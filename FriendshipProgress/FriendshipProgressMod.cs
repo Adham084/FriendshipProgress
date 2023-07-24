a5eaf5045d4d75c56dd2a2be55358dccacfd6855
Adham084 <79041023+Adham084@users.noreply.github.com>
2023-07-24 20:18:00 +0300

    initial commit

---
+++ FriendshipProgress/FriendshipProgressMod.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace FriendshipProgress
+{
+    public class FriendshipProgressMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Harmony harmony = new(ModManifest.UniqueID);
+
+            harmony.Patch(
+                AccessTools.Method(typeof(SocialPage), "drawNPCSlot"),
+                postfix: new HarmonyMethod(typeof(FriendshipProgressMod), nameof(DrawNPCSlotPostfix))
+            );
+        }
+
+        public static void DrawNPCSlotPostfix(SpriteBatch b, int i, SocialPage __instance, List<ClickableTextureComponent> ___sprites)
+        {
+            b.DrawString(
+                Game1.dialogueFont,
+                "Current Heart: " + (Game1.player.friendshipData[(string)__instance.names[i]].Points % 250) + "/250",
+                new Vector2(__instance.xPositionOnScreen + 316, ___sprites[i].bounds.Y + 8),
+                Game1.textColor
+            );
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Frame Rate Viewer
+A [SMAPI](https://smapi.io/) mod for Stardew Valley 1.5.5 that adds a fully customizable frame rate counter, because Afterburner have some issues with SMAPI.
+
+The mod can be configured via [Generic Mod Config Menu](https://www.nexusmods.com/stardewvalley/mods/5098) (Optional dependency) or just use the config file.
+
+Contributions are welcome.
+
+Download the mod from [NexusMods](https://www.nexusmods.com/stardewvalley/mods/14046).
